35313b32c4aa5cd68bbd4b7e7a2d06b6e59a697c
emilybae1705 <baemily22@gmail.com>
2025-06-24 16:38:29 -0700

    added CRUD functionality

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./status.db")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
   with Session(engine) as session:
--- app/models.py
+++ app/models.py
@@ -2,21 +2,27 @@ from sqlmodel import SQLModel, Field
 from typing import Optional
 from datetime import datetime
 
-class DeviceStatus(SQLModel):
+class DeviceStatus(SQLModel, table=True):
   id: Optional[int] = Field(primary_key=True, index=True)
   device_id: str = Field(index=True)
   timestamp: datetime
   battery_level: int = Field(ge=0, le=100)
   rssi: int
   online: bool
-  
-class DeviceStatusCreate(SQLModel):
+
+class DeviceStatusInput(SQLModel):
   device_id: str
   timestamp: datetime
   battery_level: int = Field(ge=0, le=100)
   rssi: int
   online: bool
-  
+
+class DeviceStatusUpdate(SQLModel):
+  timestamp: Optional[datetime] = None
+  battery_level: Optional[int] = Field(default=None, ge=0, le=100)
+  rssi: Optional[int] = None
+  online: Optional[bool] = None
+
 class SummaryItem(SQLModel):
   device_id: str
   last_update: datetime
